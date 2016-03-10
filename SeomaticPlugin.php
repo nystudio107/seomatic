da1e736acf7aeb7b9e39c5a657fe16d2dba8e2d4
Andrew Welch <andrew@megalomaniac.com>
2016-03-10 18:50:38 -0500

    Version 1.1.8

--- SeomaticPlugin.php
+++ SeomaticPlugin.php
@@ -25,7 +25,7 @@ class SeomaticPlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.1.7';
+        return '1.1.8';
     }
 
     public function getSchemaVersion()
