2cfafd26569b5eedbd2e3cf48ceb6bbcb8e4980e
Andrew Welch <andrew@AmbrosiaSW.com>
2016-08-01 21:17:55 -0400

    Increase HTML SimpleDom's MAX_FILE_SIZE

--- vendor/sunra/php-simple-html-dom-parser/Src/Sunra/PhpSimple/simplehtmldom_1_5/simple_html_dom.php
+++ vendor/sunra/php-simple-html-dom-parser/Src/Sunra/PhpSimple/simplehtmldom_1_5/simple_html_dom.php
@@ -67,7 +67,7 @@ define('DEFAULT_BR_TEXT', "\r\n");
 define('DEFAULT_SPAN_TEXT', " ");
 if (!defined('MAX_FILE_SIZE'))
 {
-    define('MAX_FILE_SIZE', 600000);
+    define('MAX_FILE_SIZE', 2000000);
 }
 // helper functions
 // -----------------------------------------------------------------------------
