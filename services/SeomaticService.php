58ce39a5066d8a681c56e956a14209ec7e9bc813
Andrew Welch <andrew@nystudio107.com>
2017-02-12 15:21:31 -0500

    [Improved] Fixed FieldType saving issue

--- services/SeomaticService.php
+++ services/SeomaticService.php
@@ -518,8 +518,7 @@ class SeomaticService extends BaseApplicationComponent
                 $elemType == ElementType::Category)
 */
             if ((isset($element->content) && is_object($element->content)) &&
-                (isset($element->content->attributes))
-            )
+                (isset($element->content->attributes)) && (is_object($element->content->attributes) || is_array($element->content->attributes)))
             {
                 $attributes = $element->content->attributes;
                 foreach ($attributes as $key => $value)
